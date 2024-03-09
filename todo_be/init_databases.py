ede0065aea9c77660913d59affb61b27a5b1803e
Abed Bargho <abed.bargho@verint.com>
2024-03-10 01:14:31 +0200

    Changed datetime to israel when initializing dbs

--- todo_be/init_databases.py
+++ todo_be/init_databases.py
@@ -2,6 +2,7 @@ from databases.interface import DatabaseInterface
 from databases.tasks import dbm as tasks_dbm
 from datetime import datetime
 from configparser import ConfigParser
+import pytz
 
 tasks = DatabaseInterface("tasks")
 tracker = DatabaseInterface("tracker")
@@ -23,5 +24,5 @@ if __name__ == "__main__":
     tasks_dbm.addCategory("Work")
     tasks_dbm.addTasksOrderList()
 
-    curr_day_str = datetime.now().strftime("%a %d/%m/%Y")
+    curr_day_str = datetime.now(pytz.timezone("Israel")).strftime("%a %d/%m/%Y")
     tasks.alterQuery("INSERT INTO today (current_day) VALUES (?)", (curr_day_str,))
