d5460ca6cd3de226b7320f7f56b02d23ddcd0a85
Abed Bargho <abed.bargho@verint.com>
2024-04-20 19:04:58 +0300

    changed "py" command to "python3" in pipeline

--- devops/pipelines/init_databases.sh
+++ devops/pipelines/init_databases.sh
@@ -4,7 +4,7 @@ source interface.sh
 
 log_stage "Initializing databases..."
 cd "../../${TODO_BE_FOLDER}"
-py init_databases.py
+python3 init_databases.py
 check_status "Initialized databases succesfully"
 
 read -p "Press Enter to exit..."
